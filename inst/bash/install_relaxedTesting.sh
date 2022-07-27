1c5589c6a77f1f8ba29a62b883ea43c7ba2f97bb
joshwlambert <j.w.l.lambert@rug.nl>
2022-07-27 12:24:49 +0200

    added install bash script and moved test_rr_params script

---
+++ inst/bash/install_relaxedTesting.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=23:59:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=install
+#SBATCH --output=install.log
+#SBATCH --mem=5GB
+#SBATCH --partition=gelifes
+
+mkdir -p logs
+ml R
+Rscript -e "remotes::install_github(joshwlambert/relaxedTesting)"
